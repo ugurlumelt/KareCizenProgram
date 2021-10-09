f06aa30781bfd6d123d08a714c2916ce1885745c
Meltem UGURLU <meltemak8@gmail.com>
2021-10-09 20:01:04 +0300

    Add project files.

---
+++ KareCizenProgram/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace KareCizenProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Kare oluşturmak için kenar uzunluğu için sayı girmelisiniz.");
+
+            Console.WriteLine("Kenar uzunluğu : ");
+            int kenarUzunlugu = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < kenarUzunlugu; i++)
+            {
+                for (int j = 0; j < kenarUzunlugu; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
